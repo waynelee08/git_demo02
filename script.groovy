4c63fab4ed49557afbdb70b2490aa2bcb26d24ac
waynelee08 <113997005+waynelee08@users.noreply.github.com>
2023-02-04 00:28:17 +1030

    Add files via upload

---
+++ script.groovy
@@ -0,0 +1,15 @@
+def buildApp(){
+        echo "building the app....."
+        echo "building verion ${NEW_VERSION}"
+}
+
+def testApp(){
+        echo "testing the app....."
+}
+ 
+
+def deployApp(){
+        echo "deploying the app....."
+        echo "deploying with ${SEVER_CREDENTIALS}"
+        echo "deploying version ${params.VERSION}"
+} 
